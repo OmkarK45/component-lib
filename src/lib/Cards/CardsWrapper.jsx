688a8485997719f12e7ebc5da58e78c3cbd7fdda
Omkar Kulkarni <omkarak45@gmail.com>
2021-03-17 15:53:43 +0530

    Added cards

--- src/lib/Cards/CardsWrapper.jsx
+++ src/lib/Cards/CardsWrapper.jsx
@@ -1,16 +1,24 @@
 import ComponentHolder from "../../components/Container/ComponentHolder"
 import SectionHeader from "../../components/Section/SectionHeader"
-import { Card } from "./Card"
+import { CardWithImage, CardWithImage2, TextCard } from "./Card"
 
 export default function CardsWrapper() {
   return (
     <div>
       <SectionHeader
-        title="Buttons"
-        description="The Button component is used to trigger an action or event, such as submitting a form, opening a dialog, canceling an action, or performing a delete operation."
+        title="Cards"
+        description="Used to display information to users, with button to trigger actions. These are very versatile and often found in eCommerce websites and blogs."
       />
-      <ComponentHolder title="Solid Buttons">
-        <Card />
+      <ComponentHolder title="Card with image and action button">
+        <div className="flex flex-col space-y-5 space-x-6 sm:space-y-3 items-center sm:justify-center sm:flex-row">
+          <CardWithImage image={true} />
+          <CardWithImage2 />
+        </div>
+      </ComponentHolder>
+      <ComponentHolder title="Card with text and action button">
+        <div className="flex space-x-4 items-center ">
+          <CardWithImage image={false} />
+        </div>
       </ComponentHolder>
     </div>
   )
