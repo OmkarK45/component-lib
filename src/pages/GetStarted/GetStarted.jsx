9a750e4c243dc2348adec17d5ee34f091aa765a1
Omkar Kulkarni <omkarak45@gmail.com>
2021-03-21 17:11:32 +0530

    Fixed : Download link

--- src/pages/GetStarted/GetStarted.jsx
+++ src/pages/GetStarted/GetStarted.jsx
@@ -29,18 +29,20 @@ export default function GetStarted() {
             <a
               href="/dist/css/dogeui.css"
               type="button"
+              download="dogeui.css"
               className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Download (7.26 KB)
             </a>
-            <button
+            <a
               href="/dist/css/dogeui.min.css"
               type="button"
+              download="dogeui.min.css"
               className="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
             >
               <HiDownload className="text-lg mr-2" />
               Download (minified) (5.59 KB)
-            </button>
+            </a>
           </div>
         </div>
       </div>
